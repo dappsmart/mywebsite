35a2d6ba602a2ecf314301743b037d9971a86423
gcoderz <guzidemail@gmail.com>
2022-11-24 09:42:40 +0300

    file names modified

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_+-y-0*&q(a$3ny#$0)3u94-$g4p-omk&q)irhook_w5+qk7u2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
