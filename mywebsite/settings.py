afdf28ebc9c1810ba26399510032d7dc69835110
gcoderz <guzidemail@gmail.com>
2022-11-25 19:57:33 +0300

    ...

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -133,7 +133,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
