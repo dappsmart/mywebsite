d6900ad09f7dcced782f9b3f34fa5c5d300db80e
gcoderz <guzidemail@gmail.com>
2022-11-25 10:43:18 +0300

    static_root added

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_+-y-0*&q(a$3ny#$0)3u94-$g4p-omk&q)irhook_w5+qk7u2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
